6659874a0c1c8a1fed0e2527c901611d3d0ff9f8
NgPhuongThuan <nguyenphuongthuan95@gmail.com>
2018-09-14 11:23:32 +0700

    wtf

--- server/server.js
+++ server/server.js
@@ -1,26 +1,20 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-passport.use(new GoogleStrategy());
 
 // 820849889489-2l62hgnerhtneg65uuq6lbkehhfgbhg2.apps.googleusercontent.com
 // oiaSW7ok5pKyIGNz9_7TsLCi
 
 
-
-// Route
 app.get('/', (req, res) => {
-  res.send({name: 'Carl'})
+  res.send({name: 'Carl'});
 });
 
-
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running in port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running in port ${port}`);
 });
 
